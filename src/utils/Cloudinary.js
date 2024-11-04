71d19f5393bf64ddcebfeb7e5b64c5abedfca962
ankit <aksingh9318@gmail.com>
2024-11-04 22:36:59 +0530

    all setup configured

--- src/utils/Cloudinary.js
+++ src/utils/Cloudinary.js
@@ -6,3 +6,25 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
+
+const uploadOnCloudinary = async (locaFilePath) => {
+    try {
+        if (!locaFilePath) return null 
+        // upload this file on cloudinary
+        const response = await cloudinary.uploader.upload(locaFilePath, {
+            resource_type: 'auto'
+        })
+        // file has been uplaod successfull
+        console.log("file has been uplaod on cloudinary", response.url);
+        return response
+        
+    } catch (error) {
+        fs.unlinkSync(locaFilePath) // remove the locally saved temporary file as the upload
+    }
+}
+
+
+cloudinary.v2.uploader.upload("https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg",
+    {public_id: "olympic_flag"},
+    function (error, result) {console.log(result);}
+    );
